ede6c9f5e4798c0d1bcb18f57c1667aba3d991a3
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-02 12:46:11 +0500

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,16 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+    from AKING64 import main
+    main()
+elif bit == '32bit':
+    print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+from AKING32 import main
+    main()
