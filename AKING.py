d965d5d0319a82cdca3f352fc174098cf69fdc8c
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-02 12:53:20 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,7 +5,7 @@ except:
     os.system('pip install requests')
     os.system('git pull')
     import requests
-    bit = platform.architecture()[0]
+bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
     from AKING64 import main
